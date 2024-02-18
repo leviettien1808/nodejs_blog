02fd17a5eff8877551bb12e0e000053c73c60148
Tien Le <leviettien1808@gmail.com>
2024-02-18 17:08:30 +0700

    Read data from DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,17 +1,17 @@
 const Course = require('../models/Course');
+const { mutipleMongooseToObject } = require('../../util/mongoose');
 
 class SiteController {
     // [GET] /
-    index(req, res) {
+    index(req, res, next) {
         Course.find({})
-            .then(function (courses) {
-                res.json(courses);
+            .then((courses) => {
+                res.render('home', {
+                    courses: mutipleMongooseToObject(courses),
+                });
             })
-            .catch(function (error) {
-                // res.status(400).json({error:'ERROR!!!'})
-                console.log(error);
-            });
-        // res.render('home');
+            .catch(next);
+        // ;
     }
 
     // [GET] /search
