b45182555a08d1b84137d24da82954044c6763c3
Tien Le <leviettien1808@gmail.com>
2024-02-18 09:04:09 +0700

    connet to db

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,7 +1,17 @@
+const Course = require('../models/Course');
+
 class SiteController {
     // [GET] /
     index(req, res) {
-        res.render('home');
+        Course.find({})
+            .then(function (courses) {
+                res.json(courses);
+            })
+            .catch(function (error) {
+                // res.status(400).json({error:'ERROR!!!'})
+                console.log(error);
+            });
+        // res.render('home');
     }
 
     // [GET] /search
