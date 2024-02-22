ad7bae0558236898b632a69474751fde5e01ba5f
Tien Le <leviettien1808@gmail.com>
2024-02-22 19:31:06 +0700

    Delete course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -42,6 +42,12 @@ class CourseController {
             .then(() => res.redirect('/me/stored/courses'))
             .catch(next);
     }
+    // [DELETE] /course/:id
+    destroy(req, res, next) {
+        Course.deleteOne({ _id: req.params.id }, req.body)
+            .then(() => res.redirect('back'))
+            .catch(next);
+    }
 }
 
 module.exports = new CourseController();
